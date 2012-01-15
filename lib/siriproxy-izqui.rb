f3ea1335d3f023d087debdf22d43767b3cb3dfee
izqui <izqui97@gmail.com>
2012-01-15 23:33:26 +0100

    please work

--- lib/siriproxy-izqui.rb
+++ lib/siriproxy-izqui.rb
@@ -8,7 +8,7 @@ require 'siri_objects'
 class SiriProxy::Plugin::Izqui < SiriProxy::Plugin
 
   
-  listen_for /Test for the world/i do
+  listen_for /i love you/i do
     say "Hi Jorge! That was my first Siri hack. @izqui9 :D" #say something to the user!
     
     request_completed #always complete your request! Otherwise the phone will "spin" at the user!

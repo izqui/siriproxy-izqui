28edea6e6830c03ac4c2a0b44057853d37820047
izqui <izqui97@gmail.com>
2012-01-15 23:30:16 +0100

    please, work

--- lib/siriproxy-izqui.rb
+++ lib/siriproxy-izqui.rb
@@ -4,11 +4,9 @@ require 'siri_objects'
 
 
 
-<<<<<<< HEAD
+
 class SiriProxy::Plugin::Izqui < SiriProxy::Plugin
-=======
-class SiriProxy::Plugin::siri_izqui < SiriProxy::Plugin
->>>>>>> 97c15397be4003ccea799553a6c0c9a38699d4b0
+
   
   listen_for /Test for the world/i do
     say "Hi Jorge! That was my first Siri hack. @izqui9 :D" #say something to the user!

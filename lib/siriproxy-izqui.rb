93048f14dae500122600d8404a3a1801bc876122
izqui <izqui97@gmail.com>
2012-01-19 07:58:10 +0100

    added button

--- lib/siriproxy-izqui.rb
+++ lib/siriproxy-izqui.rb
@@ -14,15 +14,10 @@ class SiriProxy::Plugin::Izqui < SiriProxy::Plugin
   listen_for /what do you think about android/i do
     response = ask "I think is crab. Have you ever tried?"
     
-    if (response =~ /yes/i)
-        
-        resp2 = ask "Tell me that you liked it and I'll kill you"
-        if (resp2 =~ /i didn't/i || resp2 =~ /no/i)
-          say "Thank God!"
-        
-        end
-    else say "You deserve to die."
-    end
+    object = SiriAddViews.new
+    object.make_root(last_ref_id)
+    button = SiriButton.new("Yes", "url:http://about.me/izqui")
+    send_objects button
     request_completed #always complete your request! Otherwise the phone will "spin" at the user!
   end
   

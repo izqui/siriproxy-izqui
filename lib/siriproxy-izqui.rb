2d639f7052965f99cd7c04047f87db34b02f365f
izqui <izqui97@gmail.com>
2012-01-19 08:07:49 +0100

    try button

--- lib/siriproxy-izqui.rb
+++ lib/siriproxy-izqui.rb
@@ -12,12 +12,11 @@ class SiriProxy::Plugin::Izqui < SiriProxy::Plugin
   end
   
   listen_for /what do you think about android/i do
-    response = ask "I think is crab. Have you ever tried?"
-    
     object = SiriAddViews.new
     object.make_root(last_ref_id)
     button = SiriButton.new("Yes", "url:http://about.me/izqui")
-    send_objects button
+    object.views << button
+    send_objects object
     request_completed #always complete your request! Otherwise the phone will "spin" at the user!
   end
   
